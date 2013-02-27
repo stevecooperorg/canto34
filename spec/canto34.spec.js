eeb9b708efdbcca7c39062a048055c62ae64fb92
Steve Cooper <steve@stevecooper.org>
2013-02-27 00:42:32 +0000

    written a basic lexer and tests.

--- spec/canto34.spec.js
+++ spec/canto34.spec.js
@@ -1,42 +1,133 @@
-var canto34 = require('../src/canto34');
 "use strict"
+var canto34 = require('../src/canto34');
 
-describe('The lexer,', function() {
+describe('When adding token types to the lexer,', function() {
 
-	describe('when adding a token type,', function() {
+	var lexer;
+	var tokenType;
 
-		var lexer;
-		var validTokenType;
+	beforeEach(function() {
+		lexer = new canto34.Lexer();
+		tokenType = {
+			name: "name",
+			regexp: / \t/
+		};
+	});
 
-		beforeEach(function() {
-			lexer = new canto34.Lexer();
-			tokenType = {
-				name: "name",
-				rexexp: / \t/g
+	it('requires token types to have names', function() {
+		expect(function() {
+			delete tokenType.name;
+			lexer.addTokenType(tokenType); 
+		}).toThrow("Token types must have a 'name' property");
+	});
+		
+	it('requires token types to have regexps', function() {
+		expect(function() {
+			delete tokenType.regexp
+			lexer.addTokenType(tokenType); 
+		}).toThrow("Token types must have a 'regexp' property");
+	});
+
+    it('requires the regexp property to be a regexp', function() {
+		expect(function() {
+			tokenType.regexp = "not a regexp";
+			lexer.addTokenType(tokenType); 
+		}).toThrow("Token types 'regexp' property must be an instance of RegExp");
+	});
+
+	it('allows an interpreter property to be a function', function() {
+		
+		expect(function() {
+			tokenType.interpreter = function() { return 0; };
+			lexer.addTokenType(tokenType);
+		}).not.toThrow();
+	});
+
+	it('requires the interpreter property to be a function', function() {
+
+		expect(function() {
+			tokenType.interpreter = "not a function";
+			lexer.addTokenType(tokenType);
+		}).toThrow("Token types 'interpreter' property must be a function");
+	});
+
+});
+
+describe('When lexing tokens', function() {
+	
+	var lexer;
+
+	beforeEach(function() {
+		lexer = new canto34.Lexer();
+	});
+
+	it('requires content to be passed', function() {
+		expect(function() {
+			lexer.tokenize(undefined);
+		}).toThrow("No content provided");
+	})
+
+	it('requires at least one token type', function() {
+		expect(function() {
+			// we've not added any token types
+			lexer.tokenize("something to tokenise");
+		}).toThrow("No token types defined");
+	})
+
+    it('can tokenize a simple example', function() {
+		lexer.addTokenType({
+			name: "A",
+			regexp: /a+/
+		});
+
+		lexer.addTokenType({
+			name: "B",
+			regexp: /b+/
+		});
+
+		var tokens = lexer.tokenize("aaabbbaaa");
+		expect(tokens).toEqual([
+			{ content: "aaa", type: "A", position:0 },
+			{ content: "bbb", type: "B", position:3 },
+			{ content: "aaa", type: "A", position:6 },
+		]);
+    })
+
+	it('allows the use of an ignore flag in token types', function() {
+
+		lexer.addTokenType({
+			name: "whitespace",
+			regexp: /[ \t]+/,
+			ignore: true
+		});
+
+
+		lexer.addTokenType({
+			name: "word",
+			regexp: /[a-z]+/,
+			ignore: false
+		});
+
+		var tokens = lexer.tokenize("aa bb cc");
+		expect(tokens).toEqual([ 
+			{ content:"aa", type:"word", position:0 },
+			{ content:"bb", type:"word", position:3 },
+			{ content:"cc", type:"word", position:6 }
+		]);
+	});
+
+	it('allows the use of custom interpreters', function() {
+		lexer.addTokenType({
+			name: "integer",
+			regexp: /\d+/,
+			interpreter: function(content) {
+				return parseInt(content);
 			}
 		});
 
-		it('requires token types to have names', function() {
-			expect(function() {
-				delete tokenType.name;
-				lexer.addTokenType(tokenType); 
-			}).toThrow("Patterns must have a 'name' property");
-		});
-			
-		it('requires token types to have regexps', function() {
-			expect(function() {
-				delete tokenType.regexp
-				lexer.addTokenType(tokenType); 
-			}).toThrow("Patterns must have a 'regexp' property");
-		});
-
-        it('requires the regexp property to be a regexp', function() {
-			expect(function() {
-				tokenType.regexp = "not a regexp";
-				lexer.addTokenType(tokenType); 
-			}).toThrow("Patterns 'regexp' property must be an instance of RegExp");
-		});
-
-	})
-
+		var tokens = lexer.tokenize("123");
+		expect(tokens).toEqual([
+			{ content: 123, type:"integer", position:0 }
+		]);
+	});
 });
--- src/canto34.js
+++ src/canto34.js
@@ -10,27 +10,94 @@
          }
     **/
 	canto34.Lexer = function() {
-		this.patterns = [];
+		this.tokenTypes = [];
 	};
 
     var PatternDefinitionException = function(message) {
     	this.message = message;
     }
 
+    var LexerException = function(message) {
+    	this.message = message;
+    }
+
 	canto34.Lexer.prototype = {
-		addTokenType: function(pattern) {
-			if (!pattern.name) {
-				throw new PatternDefinitionException("Patterns must have a 'name' property");
+		addTokenType: function(tokenType) {
+			//console.log(tokenType);
+
+			if (!tokenType.name) {
+				throw new PatternDefinitionException("Token types must have a 'name' property");
 			}
 
-			if (!pattern.regexp) { 
-				throw new PatternDefinitionException("Patterns must have a 'regexp' property");
+			if (!tokenType.regexp) { 
+				throw new PatternDefinitionException("Token types must have a 'regexp' property");
 			}
 
 
-			if (!(pattern.regexp instanceof RegExp)) { 
-				throw new PatternDefinitionException("Patterns 'regexp' property must be an instance of RegExp");
+			if (!(tokenType.regexp instanceof RegExp)) { 
+				throw new PatternDefinitionException("Token types 'regexp' property must be an instance of RegExp");
 			}
+
+			if(tokenType.interpreter && typeof tokenType.interpreter !== "function") {
+				throw new PatternDefinitionException("Token types 'interpreter' property must be a function");
+			}
+			this.tokenTypes.push(tokenType);
+		},
+		tokenize: function(content) {
+			if (content === undefined) {
+				throw new LexerException("No content provided");
+			}
+
+			if (this.tokenTypes.length === 0) {
+				throw new LexerException("No token types defined");
+			}
+
+			var result = [];
+
+			var remaining = content;
+			var position = 0;
+			var tokenTypeLength = this.tokenTypes.length;
+
+			while(remaining.length > 0) {
+				var somethingFoundThisPass = false;
+
+				for (var i = 0; i < tokenTypeLength; i++) {
+					var tokenType = this.tokenTypes[i];
+					var match = tokenType.regexp.exec(remaining);
+					if (match) {
+						// we found a token! great. What did it say? We only
+						// want to match at the start of the string
+						if (match.index == 0) {
+							somethingFoundThisPass = true;
+							var content = match[0];
+							remaining = remaining.substring(content.length);
+							
+							if (tokenType.interpreter) {
+								content = tokenType.interpreter(content);
+							}
+
+							var token = {
+								content: content,
+								type: tokenType.name,
+								position: position
+							};
+
+							if (!tokenType.ignore) {
+								result.push(token);
+							}
+
+							position += content.length;
+							continue;
+						}
+					}
+				}
+
+				if (!somethingFoundThisPass) {
+					throw new Lexerexception("No viable alternative at '" + remaining.substring(0, 15));
+				}
+			}
+
+			return result;
 		}
 	};
 
